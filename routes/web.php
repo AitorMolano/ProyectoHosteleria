4bd30def8e9666cd071cc76245cfbb7830b2deea
AitorMolano <aitor.molano@ikasle.egibide.org>
2022-03-04 08:39:58 +0100

    preMerge

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('productos/{id}', [ProductoController::class, 'show'])->name('detalleProd');
+
+
+
 Auth::routes();
 
 //Route::get('/', [ProductoController::class, 'index'])->name('productos');
