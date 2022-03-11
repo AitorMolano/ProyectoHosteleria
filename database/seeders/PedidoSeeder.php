9070b4b4d70f0af78f25064ea5748669a66e3f6f
ALAITZ CANDELA MURELAGA <alaitz.candela@ikasle.egibide.org>
2022-03-11 19:06:45 +0100

    deploy ns cuanto 8.1

--- database/seeders/PedidoSeeder.php
+++ database/seeders/PedidoSeeder.php
@@ -27,7 +27,7 @@ class PedidoSeeder extends Seeder
         ]);
         DB::table('pedidos')->insert([
             'id_cliente' =>61,
-            'suma_precio'=>31,
+            'suma_precio'=>21,
             'estado'=>'en proceso'
         ]);
     }
