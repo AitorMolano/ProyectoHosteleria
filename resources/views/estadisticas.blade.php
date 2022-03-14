822c1c960b368dd190c60d358179d7110ca2ed98
AitorMolano <aitor.molano@ikasle.egibide.org>
2022-03-14 09:33:34 +0100

    hotfix 5

--- resources/views/estadisticas.blade.php
+++ resources/views/estadisticas.blade.php
@@ -5,8 +5,8 @@
     $cantidad= count($pedidos);
     $cantidad0= count($enviados);
     $cantidad1= count($en_cursos);
-    $cantidad2= count($pedidos);
-    $cantidad3= count($en_caminos);
+    $cantidad2= count($en_caminos);
+    $cantidad3= count($pedidos);
     $cantidad4= count($c_recibidos);
     $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
     $contar =1;
@@ -16,18 +16,22 @@
         </figure>
     
         <h3>Pedidos actuales</h3>
+        <div class="row">
         <table class="table table-striped table-hover table-responsive">
         <tr>
+                <th>Enviados</th>
                 <th>En curso</th>
                 <th>Preparados</th>
                 <th>Recibidos</th>
             </tr>
             <tr>
+                <th>{{$cantidad0}}</th>
                 <th>{{$cantidad1}}</th>
                 <th>{{$cantidad2}}</th>
                 <th>{{$cantidad3}}</th>
             </tr>
         </table>
+        </div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
         <script src="{{ asset('/js/grafico.js') }}"></script>
