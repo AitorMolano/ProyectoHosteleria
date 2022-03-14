aa671126048bea5454024e6faf05e67076e78de0
AitorMolano <aitor.molano@ikasle.egibide.org>
2022-03-14 09:10:49 +0100

    hotfix1

--- app/Http/Controllers/EstadisticaController.php
+++ app/Http/Controllers/EstadisticaController.php
@@ -29,8 +29,9 @@ class EstadisticaController extends Controller
         $pedidos = Pedido::all();
         $hoy = date('Y-m-d ');
         $ayer = date('Y-m-d', strtotime ( '- 1 month' , strtotime ( $hoy ) ));
+        $enviados = Pedido::where('estado','producto enviado')->whereDate('created_at','>=',$ayer)->whereDate('created_at','<',$hoy)->get();
         $en_cursos = Pedido::where('estado','en proceso')->whereDate('created_at','>=',$ayer)->whereDate('created_at','<',$hoy)->get();
-        $preparados = Pedido::where('estado','preparado')->whereDate('created_at','>=',$ayer)->whereDate('created_at','<',$hoy)->get();
+        $en_caminos = Pedido::where('estado','en camino')->whereDate('created_at','>=',$ayer)->whereDate('created_at','<',$hoy)->get();
         $recibidos = Pedido::where('estado','recibido')->whereDate('created_at','>=',$ayer)->whereDate('created_at','<',$hoy)->get();
         
         $meses = [36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1];
@@ -41,7 +42,7 @@ class EstadisticaController extends Controller
         foreach($meses as $el_mes){
             
             $mes_1 = date('Y-m-d', strtotime ( '-'.$el_mes.' month' , strtotime ( $hoy ) ));
-            $mes = date('Y-m-d', strtotime ( '- 1 month' , strtotime ( $mes_1 ) ));
+            $mes = date('Y-m-d', strtotime ( '+ 1 month' , strtotime ( $mes_1 ) ));
             $n_mes=date("n",strtotime($mes_1));
             
             if($ya ==1){
@@ -58,9 +59,10 @@ class EstadisticaController extends Controller
         
         return view('estadisticas', [
             'c_recibidos' =>  $c_recibidos,
+            'enviados' => $enviados,
             'pedidos' => $pedidos,
             'en_cursos' => $en_cursos,
-            'preparados' => $preparados,
+            'en_caminos' => $en_caminos,
             'recibidos' => $recibidos,
         ]);
     }
