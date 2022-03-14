6537376f0c2e368ca2ba699b36186736738ca8fd
AitorMolano <aitor.molano@ikasle.egibide.org>
2022-03-14 10:10:25 +0100

    hotfix 10

--- app/Http/Controllers/EstadisticaController.php
+++ app/Http/Controllers/EstadisticaController.php
@@ -34,31 +34,7 @@ class EstadisticaController extends Controller
         $en_caminos = Pedido::where('estado','en camino')->whereDate('created_at','>=',$ayer)->whereDate('created_at','<',$hoy)->get();
         $recibidos = Pedido::where('estado','recibido')->whereDate('created_at','>=',$ayer)->whereDate('created_at','<',$hoy)->get();
         
-        $meses = [36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1];
-        $c_en_cursos = array();
-        $c_preparados = array();
-        $c_recibidos = array();
-        $ya = 0;
-        foreach($meses as $el_mes){
-            
-            $mes_1 = date('Y-m-d', strtotime ( '-'.$el_mes.' month' , strtotime ( $hoy ) ));
-            $mes = date('Y-m-d', strtotime ( '- 1 month' , strtotime ( $mes_1 ) ));
-            $n_mes=date("n",strtotime($mes_1));
-            
-            if($ya ==1){
-            $el_recibido = Pedido::where('estado','recibido')->whereDate('created_at','>=',$mes_1)->whereDate('created_at','<',$mes)->get();
-            $c_recibido[0] = $mes_1;
-            $c_recibido[1] = $el_recibido;
-            if($c_recibido[1])
-                array_push($c_recibidos,$c_recibido);
-            }
-            if( $n_mes == 11){
-                $ya=1;
-            }
-        }
-        
         return view('estadisticas', [
-            'c_recibidos' =>  $c_recibidos,
             'enviados' => $enviados,
             'pedidos' => $pedidos,
             'en_cursos' => $en_cursos,
