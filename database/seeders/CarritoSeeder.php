f5aaa0b23b60a49edd6b8ea03ad2c250a01039c2
ALAITZ CANDELA MURELAGA <alaitz.candela@ikasle.egibide.org>
2022-03-14 21:29:05 +0100

    ª

--- database/seeders/CarritoSeeder.php
+++ database/seeders/CarritoSeeder.php
@@ -29,16 +29,16 @@ class CarritoSeeder extends Seeder
         ]);
 
         DB::table('carritos')->insert([
-            'id_producto'=>34,
+            'id_producto'=>331,
             'id_pedido'=>11,
         ]);
         DB::table('carritos')->insert([
-            'id_producto'=>32,
+            'id_producto'=>311,
             'id_pedido'=>11,
         ]);
 
         DB::table('carritos')->insert([
-            'id_producto'=>32,
+            'id_producto'=>311,
             'id_pedido'=>21,
         ]);
         
