79da522e3d77e513fcc48ebaca7e5576f31f3447
AitorMolano <aitor.molano@ikasle.egibide.org>
2022-03-15 09:28:33 +0100

    foreign

--- database/migrations/2022_03_02_112458_create_carritos_table.php
+++ database/migrations/2022_03_02_112458_create_carritos_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('carritos', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_producto');
-            $table->foreign('id_producto')->references('id')->on('productos');
+            $table->foreign('id_producto')->references('id')->on('productos')->cascadeOnDelete();
             $table->unsignedBigInteger('id_pedido');
-            $table->foreign('id_pedido')->references('id')->on('pedidos');
+            $table->foreign('id_pedido')->references('id')->on('pedidos')->cascadeOnDelete();
             $table->timestamps();
         });
         Schema::enableForeignKeyConstraints();
